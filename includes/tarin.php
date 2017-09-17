b4fb2313b197008ee89b96278bc9b6aef0e0d716
tarinrickett <trickett96@gmail.com>
2017-09-17 18:44:55 -0400

    tarin profile 1.0

---
+++ includes/tarin.php
@@ -0,0 +1,76 @@
+    <section>
+      <h1>Hello, World</h1>
+      <p>I am a junior at the University of Rochester in upstate New York pursuing a double major in Computer Science and Brain & Cognitive Sciences with a strong range of prior internship experience in web development and design.</p>
+      <p>I am always looking for fresh and innovative challenges to expand my development skills. A dedicated and enthusiastic worker, I am passionate about intelligent design and a quick learner always seeking new opportunities to learn and grow.</p>
+    </section>
+
+
+    <section>
+      <h1><a id="Experience">Experience</a></h1>
+      <h2>UI Engineer Intern</h2>
+      <h3>LinkedIn</h3>
+      <h4>Sunnyvale, CA &nbsp; | &nbsp; June 2017 - Sept 2017</h4>
+      <p>Excited to announce my UI Engineering internship with LinkedIn for Summer 2017!</p>
+      <a href="https://studentcareers.linkedin.com/internships" role="button">Learn more »</a>
+      <a href="http://www.linkedin.com/" role="button">View site »</a>
+    </section>
+
+
+    <section>
+      <h1><a id="Education">Education</a></h1>
+
+			<h2>University of Rochester</h2>
+			<h3>Rochester, NY &nbsp; | &nbsp; Graduate May 2018</h3>
+			<h4>B.A. Computer Science, B.A. Brain and Cognitive Sciences</h4>
+			<ul>
+				<li>Minors: Psychology, French</li>
+				<li>3.9 GPA; Dean's List all semesters</li>
+				<li>George Eastman Young Leaders Scholarship recipient</li>
+			</ul>
+
+      <h2>Université de Nantes</h2>
+      <h3>Nantes, France &nbsp; | &nbsp; Jan 2017 - May 2017</h3>
+      <h4>IES French Language Immersion & Area Studies Program</h4>
+      <ul>
+        <li>Immersive study abroad for the 2017 spring semester</li>
+        <li>Took university courses while immersing myself in the culture and language of western France</li>
+        <li>Read more about my experiences on <a href="http://tarinrickett.com/abroad/en.html">my travel blog »</a></li>
+      </ul>
+    </section>
+
+
+    <section>
+      <h1><a id="Skills">Skills</a></h1>
+      <h2>Proficient in:</h2>
+      <ul>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>Java</li>
+        <li>Javascript</li>
+        <li>Wordpress</li>
+      </ul>
+
+      <h2>Experience with:</h2>
+      <ul>
+        <li>Adobe Creative Suite</li>
+        <li>AJAX</li>
+        <li>Bootstrap</li>
+        <li>Chrome Dev Tools</li>
+        <li>Github</li>
+        <li>Handlebars</li>
+        <li>jQuery</li>
+        <li>JSDoc</li>
+        <li>Node.js</li>
+        <li>PHP</li>
+        <li>Python</li>
+        <li>Swift</li>
+        <li>Unix</li>
+        <li>Working French</li>
+      </ul>
+     
+      <h2>Currently learning:</h2>
+      <ul>
+        <li>Ember</li>
+        <li>Sass</li>
+      </ul>
+    </section>
---
+++ tarin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Tarin Rickett - Aspiring Web Developer">
+    <meta name="author" content="Tarin Rickett">
+    <title>Tarin Rickett</title>
+  </head>
+
+  <body>
+
+    <nav>
+      <ul>
+        <li><a href="#Experience">Experience</a></li>
+    		<li><a href="#Education">Education</a></li>
+    		<li><a href="#Skills">Skills</a></li>
+      </ul>
+    </nav>
+
+    <?php include "includes/hw1.php" ?>
+
+    <footer>
+      <p>Copyright © 2017 Tarin Rickett</p> 
+    </footer>
+
+  </body>
+</html>
+

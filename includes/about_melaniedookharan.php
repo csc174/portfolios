3d0b9b835e34a22e9607075d8d567d1c727026ed
mdooks <mdookhar@u.rochester.edu>
2017-09-17 18:52:38 -0400

    my about file

---
+++ includes/about_melaniedookharan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset = "utf-8">
+	<title> Melanie D. Dookharan</title>
+</head>
+
+<body>
+	<h1> Welcome to My Website</h1>
+	<p> Hi! My name is <strong>Melanie Dookharan</strong> and I'm currently a junior at the University of Rochester, majoring in Computer Science with a minor in Business. I'm particularly interested in UX/UI design and applications in Human Computer Interactions. I self learned Adobe Photoshop and Illustrator in 6th grade for a project, and have longed for a career where I can focus on design. After taking computer science courses for a little over a year, I am proficient in Python, Java, HTML, and CSS. I'm excited to combine my skills for a long-term career. Eventually, I hope to exclusively work on websites and mobile interfaces, but focus on the research and design side of it. <br> <br> In my free time, I enjoy trying new restaurants, photgraphy, drawing, and singing. In middle school, up until high school, I was focused on theater, with my first career choice being an actress. Once I got to high school, I started to do news broadcasting, genomic research, advertising, and was involved in sports. While I wanted to be a doctor my whole life, college made me realize how I had interests in other fields, like design and business. As my eventual goal was to be an entrepreneur, I didn't hesitate to drop science courses for a different major. In the future I hope to get either an MBA or a Master's in Design -- maybe both!</p>
+
+	<footer> By Melanie Dookharan, CSC 174</footer>
+
+</body>
+</html>
+
+
+
+
+
+

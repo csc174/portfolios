3aaf53dc679f16fcc94bfe47bd1d1c03282f2de2
tarinrickett <trickett96@gmail.com>
2017-09-17 19:02:02 -0400

    update file structure

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="CSC174">
+    <meta name="author" content="CJ, Mel, Kalila, Tarin">
+    <title>CSC174</title>
+  </head>
+
+  <body>
+
+    <nav>
+      <ul>
+        <li><a href="#CJ">CJ</a></li>
+    		<li><a href="#Mel">Mel</a></li>
+    		<li><a href="#Kalila">Kalila</a></li>
+        <li><a href="#Tarin">Tarin</a></li>
+      </ul>
+    </nav>
+
+    <?php include "includes/cj.php" ?>
+    <?php include "includes/mel.php" ?>
+    <?php include "includes/kalila.php" ?>
+    <?php include "includes/tarin.php" ?>
+
+    <footer>
+      <p>Copyright © 2017</p> 
+    </footer>
+
+  </body>
+</html>
+

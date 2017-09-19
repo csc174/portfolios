af34cc32365bb533abdb87ade966fbe7c9e54827
mdooks <mdookhar@u.rochester.edu>
2017-09-19 15:05:31 -0400

    Updated Info, Made more resume link

--- includes/mel.php
+++ includes/mel.php
@@ -1,4 +1,50 @@
 <section id="mel" style="display: none;">
 	<h1> Welcome to My Website</h1>
 	<p> Hi! My name is <strong>Melanie Dookharan</strong> and I'm currently a junior at the University of Rochester, majoring in Computer Science with a minor in Business. I'm particularly interested in UX/UI design and applications in Human Computer Interactions. I self learned Adobe Photoshop and Illustrator in 6th grade for a project, and have longed for a career where I can focus on design. After taking computer science courses for a little over a year, I am proficient in Python, Java, HTML, and CSS. I'm excited to combine my skills for a long-term career. Eventually, I hope to exclusively work on websites and mobile interfaces, but focus on the research and design side of it. <br> <br> In my free time, I enjoy trying new restaurants, photgraphy, drawing, and singing. In middle school, up until high school, I was focused on theater, with my first career choice being an actress. Once I got to high school, I started to do news broadcasting, genomic research, advertising, and was involved in sports. While I wanted to be a doctor my whole life, college made me realize how I had interests in other fields, like design and business. As my eventual goal was to be an entrepreneur, I didn't hesitate to drop science courses for a different major. In the future I hope to get either an MBA or a Master's in Design -- maybe both!</p>
+
+	<h1> Resume </h1>
+	<a href="http://www.melanie-dookharan.com" role="button"> * Portfolio * </a>
+	<a href="https://www.linkedin.com/in/melaniedookharan/" role= "button"> *LinkedIn* </a>
+
+	<h2>Education</h2>
+	<h3> University of Rochester</h3>
+	<h4> B.A. Computer Science (Human Computer Interaction Track), Minor in Business '19</h4>
+	<ul>
+		<li> Deans List 3/4 Semesters</li>
+		<li> Dean's Scholarship</li>
+		<li> Women's River Campus and Eastman Choir</li>
+		<li> Alpha Kappa Psi Proessional Business Fraternity</li>
+	</ul>
+
+	<h2> Work Experience</h2>
+	<h3> CSC 161 Workshop Leader | January 2017 - Present</h3>
+	<h4> University of Rochester Computer Science Department</h4>
+	<ul>
+		<li> Leads a 12 person workshop on Python concepts learned in class through problems sets and active discussions</li>
+		<li> Grades quizzes and attends a weekly course to effictively teach</li>
+	</ul>
+
+	<h3> Webmaster, Social Chair | January 2016 - Present</h3>
+	<h4> Alpha Kappa Psi, Psi Mu Chapter | University of Rochester </h4>
+	<ul>
+		<li> Resdesigned entire website to improve the UX, making the navigation more efficient and informative for potential members</li>
+		<li> Changed web hosting to allow for easier updating when future webmasters transition to the role</li>
+		<li> Collaborated with VP Alumni Relations to create and updated Alumni Database to improve internal relations with members</li>
+	</ul>
+
+	<h3> UX/UI Design Summer Intern | June 2017 - August 2017 | New York City, NY</h3>
+	<h4> Edu.chat, Inc.</h4>
+	<ul>
+		<li> Developed wireframes and high fidelity prototypes for iOs, Android, and Web platforms</li>
+		<li> Worked closely with developers to ensure prototypes were practical and screens were developed to standards</li>
+		<li> Closely followed Material Guidelines throughout Android and Web platforms</li>
+	</ul>
+
+	<h3> Women in Technology Consulting Conference | July 2017 </h3>
+	<h4> EY (Formerly Ernest & Young)</h4>
+	<ul> 
+		<li> Chosen as one of 60 women to attend a conference in Chicago, Illinois focused on the diversity and opportunities available at EY. </li>
+	</ul>
+	
+
 </section>

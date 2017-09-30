75c511b1c09416e839410aab24cdab2fd2061ba6
tarinrickett <trickett96@gmail.com>
2017-09-30 14:34:56 -0400

    replace bootstrap styles with materialize

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
   <meta content="CSC174" name="description">
   <meta content="CJ, Mel, Kalila, Tarin" name="author">
   <title>CSC174 Assignment 4</title>
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/overrides.css" rel="stylesheet">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 <body class="home">
@@ -67,8 +71,10 @@
 
   <footer>
     <p>Copyright © 2017</p>
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
   </footer>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
- 
+
 </body>
 </html>

56fc4add12c5e63079ea31208dbe3e5fa2dd9600
mdooks <mdookhar@u.rochester.edu>
2017-09-30 16:41:52 -0400

    Removed all bootstrap and related code

--- index.php
+++ index.php
@@ -14,60 +14,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
-<body class="home">
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-    <ul class="nav navbar-nav">
-      <li>
-        <a class="gov" href="index.php">Home</a>
-      </li>
-      <li>
-        <a class="gov" href="includes/mel.php" id="mel-tab">Mel</a>
-      </li>
-      <li>
-        <a class="gov" href="includes/kalila.php" id="kalila-tab">Kalila</a>
-      </li>
-      <li>
-        <a class="gov" href="includes/tarin.php" id="tarin-tab">Tarin</a>
-      </li>
-    </ul>
-  </nav>
+<body
 
-  <div class="welcome">
-    <h1>Welcome to Queens, NY</h1>
-    <h2>Click on a person to learn more about them!</h2>
-  </div>
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-4 media well" id="mel">
-        <div class="media-left">
-          <a href="includes/mel.php"><img alt="Cinque Terre" class="img-circle media-object" height="120" src="mel-headshot.jpg" width="100"></a>
-        </div>
-        <div class="media-body">
-          <h2>Mel Dookharan</h2>
-          <h3>Class of 2019</h3>
-        </div>
-      </div>
-      <div class="col-md-4 media well" id="tarin">
-        <div class="media-left">
-          <a href="includes/tarin.php"><img alt="Cinque Terre" class="img-circle media-object" height="120" src="tarin-headshot.jpg" width="100"></a>
-        </div>
-        <div class="media-body">
-          <h2>Tarin Rickett</h2>
-          <h3>Class of 2018</h3>
-        </div>
-      </div>
-      <div class="col-md-4 media well" id="kalila">
-        <div class="media-left">
-          <a href="includes/kalila.php"><img alt="Cinque Terre" class="img-circle media-object" height="120" src="kalila-headshot.jpg" width="100"></a>
-        </div>
-        <div class="media-body">
-          <h2>Kalila Shapiro</h2>
-          <h3>Class of 2018</h3>
-        </div>
-      </div>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Home</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="tarin.php">Tarin</a></li>
+        <li><a href="kalila.php">Kalila</a></li>
+        <li><a href="mel.php">Mel</a></li>
+      </ul>
     </div>
-  </div>
+  </nav>
 
   <footer>
     <p>Copyright © 2017</p>

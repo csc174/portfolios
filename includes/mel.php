8dca8438a479e62169b87a022d1af1a655f17fca
tarinrickett <trickett96@gmail.com>
2017-10-05 00:22:32 -0400

    clean up directory

--- includes/mel.php
+++ includes/mel.php
@@ -86,4 +86,13 @@
 		</div>
 	</div>
 
+	<footer>
+		<p>Copyright © 2017</p>
+		<!--Import jQuery before materialize.js-->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="../js/materialize.min.js"></script>
+	</footer>
+
+</body>
+
 </html>
